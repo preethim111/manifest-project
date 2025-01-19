32aef7af51cde3f42719dd48e1758e1e6737f10d
Cody Epstein <codyepstein@gmail.com>
2025-01-19 09:34:27 -0800

    add multer to be able to send images to the backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,150 +1,163 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import User from './model/userModel.js';
-import cors from 'cors';
-import bcrypt from 'bcrypt';
-import bodyParser from 'body-parser';
-import Board from './model/boardModel.js';
-// import firebaseAdmin from './config/firebase'; 
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import User from "./model/userModel.js";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
+import Board from "./model/boardModel.js";
+import multer from "multer";
+// import firebaseAdmin from './config/firebase';
 // import admin from './config/firebase';
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type'], // Specify allowed headers
-  }));
+app.use(
+	cors({
+		origin: "http://localhost:5173", // Allow requests from your frontend's origin
+		methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+		allowedHeaders: ["Content-Type"], // Specify allowed headers
+	})
+);
 
-app.use(express.json({ limit: '10mb' })); // Adjust the limit as needed
+app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
 
 dotenv.config();
 
 connectDB();
 
 // Middleware to parse JSON requests
-app.use(express.json())
+app.use(express.json());
 
+// Update this to include the full URL path when serving static files
+app.use("/uploads", express.static("uploads"));
+
+// Configure multer middleware for handling file uploads
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./uploads/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
 
 // POST: Endpoint to authenticate user during sign-in
-app.post('/api/authenticate', async (req, res) => {
-    const { email, password } = req.body;
+app.post("/api/authenticate", async (req, res) => {
+	const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ email })
+	try {
+		const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(404).json({ error: "User not found" })
-        }
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-        const isPasswordValid = await bcrypt.compare(password, user.password)
+		const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid credentials' })
-        }
+		if (!isPasswordValid) {
+			return res.status(401).json({ error: "Invalid credentials" });
+		}
 
-        res.status(200).send({ message: "User found" })
-    } catch (err) {
-        res.status(500).send({ error: error.message })
-    }
-})
-
+		res.status(200).send({ message: "User found" });
+	} catch (err) {
+		res.status(500).send({ error: error.message });
+	}
+});
 
 // POST: Endpoint to sign up (with hashed password)
-app.post('/api/signup', async (req, res) => {
-    const { email, password, fullName } = req.body;
-    console.log("Received user data:", req.body);
-    try {
-        const hashed = await bcrypt.hash(password, 10)
-        const user = new User({ email, password: hashed, fullName })
-        await user.save();
-        res.status(201).json({ message: "User signed up successfully" })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-
-})
-
+app.post("/api/signup", async (req, res) => {
+	const { email, password, fullName } = req.body;
+	console.log("Received user data:", req.body);
+	try {
+		const hashed = await bcrypt.hash(password, 10);
+		const user = new User({ email, password: hashed, fullName });
+		await user.save();
+		res.status(201).json({ message: "User signed up successfully" });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+});
 
 // POST: Endpoint to hash the new password after resetting
-app.post('/api/reset', async (req, res) => {
-    const { email, newPassword } = req.body;
+app.post("/api/reset", async (req, res) => {
+	const { email, newPassword } = req.body;
 
-    try {
-        const newHashed = await bcrypt.hash(newPassword, 10)
-        const user = await User.findOneAndUpdate(
-            { email },
-            { password: newHashed },
-            { new: true }
-        )
-        
-        if (!user) {
-            return res.status(404).json({ error: "User not found" })
-        }
+	try {
+		const newHashed = await bcrypt.hash(newPassword, 10);
+		const user = await User.findOneAndUpdate(
+			{ email },
+			{ password: newHashed },
+			{ new: true }
+		);
 
-        res.status(200).json({ message: "Password successfully reset" })
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    
-    }
-})
-
+		res.status(200).json({ message: "Password successfully reset" });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
 
 // POST: Endpoint to save the vision boards (not pictures, just the boards)
-app.post('/api/visionBoard', async (req, res) => {
-    const { title, description } = req.body;
+app.post(
+	"/api/visionBoard",
+	upload.single("uploadedPreviewImage"),
+	async (req, res) => {
+		const { title, description } = req.body;
+		// Create the full URL path for the image
+		const previewImage = req.file ? `/uploads/${req.file.filename}` : null;
 
-    // const token = req.headers.authorization?.split(' ')[1]; // Extract token
+		// const token = req.headers.authorization?.split(' ')[1]; // Extract token
 
-    // if (!token) {
-    //     return res.status(403).json({ message: 'No token provided' });
-    // }
+		// if (!token) {
+		//     return res.status(403).json({ message: 'No token provided' });
+		// }
 
-    try {
-        // const decoded = await admin.auth().verifyIdToken(token);
-        // const userId = decoded.uid;
+		try {
+			// const decoded = await admin.auth().verifyIdToken(token);
+			// const userId = decoded.uid;
 
-        const board = new Board({
-            title,
-            description,
-            // user: userId
-            // previewImage
-        })
+			const board = new Board({
+				title,
+				description,
+				// user: userId
+				previewImage,
+			});
 
-        await board.save()
+			await board.save();
 
-        res.status(201).json({
-            message: 'Board created successfully!',
-            board
-        });
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: "Error creating the board" })
-    }
-})
-
-
+			res.status(201).json({
+				message: "Board created successfully!",
+				board,
+			});
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: "Error creating the board" });
+		}
+	}
+);
 
 // GET: Get all boards
-app.get('/api/getboards', async (req, res) => {
-    try {
-        const boards = await Board.find()
-        res.json({ boards })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: 'Failed to fetch boards' });
-    }
-})
-
+app.get("/api/getboards", async (req, res) => {
+	try {
+		const boards = await Board.find();
+		res.json({ boards });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Failed to fetch boards" });
+	}
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`);
 });
-
 
 // POST: Endpoint to save the vision boards (not pictures, just the boards)
 // app.post('/api/visionBoard', async (req, res) => {
@@ -178,4 +191,3 @@ app.listen(PORT, () => {
 //         res.status(500).json({ message: "Error creating the board" });
 //     }
 // });
-
