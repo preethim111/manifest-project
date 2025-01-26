bf5ce8ce4cf2ee62eabebbcd7334be7000138296
preethim111 <preethimanne11@gmail.com>
2025-01-25 16:20:33 -0800

    fixing deployment

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ app.use(
 
 app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
 
-mongoose.connect('mongodb+srv://pmanne:Srisairam11$@manifest-app.svp7u.mongodb.net/test?retryWrites=true&w=majority&appName=manifest-app')
+// mongoose.connect('mongodb+srv://pmanne:Srisairam11$@manifest-app.svp7u.mongodb.net/test?retryWrites=true&w=majority&appName=manifest-app')
 
 dotenv.config();
 
